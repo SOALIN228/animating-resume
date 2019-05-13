009ebb78c477b5a3e60d921a81fa943b64d96794
lml <soalin228@163.com>
2019-05-13 19:47:44 +0800

    完善动画和样式

--- js/main.js
+++ js/main.js
@@ -16,10 +16,12 @@ let result =
    border: 1px solid #aaa;
    padding: 16px;
  }
+ 
  /* 我需要一点代码高亮 */
  .token.selector{ color: #a6e22e; }
  .token.punctuation{ transition: all 0s; color: #f8f8f2;}
  .token.property{ color: #f92672; }
+ 
  /* 加一个呼吸效果 */
  #code{
    animation: breath 0.5s infinite alternate-reverse;
@@ -28,21 +30,34 @@ let result =
  
  /* 我需要一张白纸 */
  #code-wrapper{
-  width: 50%; 
-  left: 0; 
-  position: fixed; 
-  height: 100%;
-}
-#paper > .content {
- display: block;
-}
+   width: 50%; 
+   left: 0; 
+   position: fixed; 
+   height: 100%;
+ }
+ #paper > .content {
+   display: block;
+ }
+ 
+ 
+ /* 于是我就可以在白纸上写字了，请看右边 */
+ 
+ 
  `
 
 let result2 =
-  `/* 接下来用一个优秀的库 marked.js
+  `/* 
+ * 接下来用一个优秀的库 marked.js
  * 把 Markdown 变成 HTML
  */
+ 
  `
+
+let result3 =
+  `/*
+ * 这就是我的会动的简历
+ * 谢谢观看
+ */`
 
 let md = `
 # 自我介绍
@@ -53,10 +68,12 @@ XXX 学校毕业
 希望应聘前端开发岗位
 # 技能介绍
 熟悉 JavaScript CSS
+
 # 项目介绍
 1. XXX 轮播
 2. XXX 简历
 3. XXX 画板
+
 # 联系方式
 - QQ xxxxxxxx
 - Email xxxxxxxx
@@ -78,26 +95,7 @@ function writeCode (prefix, code, fn) {
       window.clearInterval(id)
       fn &&  fn.call() // call过去
     }
-  }, 10)
-
-}
-
-writeCode('', result, () => {
-  createPaper(() => { // 接收call过来的函数
-    writeCode(result, result2, () => {
-      writeMarkdown(md)
-    })
-  })
-})
-
-function createPaper (fn) {
-  let paper = document.createElement('div')
-  paper.id = 'paper'
-  let content = document.createElement('pre')
-  content.className = 'content'
-  paper.appendChild(content)
-  document.body.appendChild(paper)
-  fn.call()
+  }, 70)
 }
 
 function writeMarkdown(markdown, fn){
@@ -112,4 +110,37 @@ function writeMarkdown(markdown, fn){
       fn && fn.call()
     }
   }, 35)
+}
+
+writeCode('', result, () => {
+  createPaper(() => { // 接收call过来的函数
+    writeMarkdown(md, () => {
+      writeCode(result, result2, () => {
+        convertMarkdownToHtml(() => {
+          writeCode(result + result2, result3, () => {
+            console.log('完成')
+          })
+        })
+      })
+    })
+  })
+})
+
+function createPaper (fn) {
+  let paper = document.createElement('div')
+  paper.id = 'paper'
+  let content = document.createElement('pre')
+  content.className = 'content'
+  paper.appendChild(content)
+  document.body.appendChild(paper)
+  fn.call()
+}
+
+function convertMarkdownToHtml(fn){
+  let div = document.createElement('div')
+  div.className = 'html markdown-body'
+  div.innerHTML = marked(md)
+  let markdownContainer = document.querySelector('#paper > .content')
+  markdownContainer.replaceWith(div)
+  fn && fn.call()
 }
